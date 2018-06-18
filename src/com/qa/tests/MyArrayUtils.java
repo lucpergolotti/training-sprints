1fff8f30eb4dfbcb4caae1461971272155abf32f
Anil <ANILA@science.regn.net>
2018-06-18 14:08:12 -0400

    added test cases for empty and many

--- src/com/qa/tests/MyArrayUtils.java
+++ src/com/qa/tests/MyArrayUtils.java
@@ -2,8 +2,18 @@ package com.qa.tests;
 
 public class MyArrayUtils {
 	public static int findHighest(int[] numbers) {
-		
-		
-		return 0;
+		int max  = 0;
+		if(numbers.length == 0) {
+			max = Integer.MIN_VALUE;
+		}
+		if(numbers.length > 2) {
+			max = numbers[0];
+			for(int i =1; i<numbers.length; i++) {
+				if(numbers[i]> max) {
+					max= numbers[i];
+				}
+			}
+		}
+		return max;
 	}
 }
--- src/com/qa/tests/MyArrayUtilsTest.java
+++ src/com/qa/tests/MyArrayUtilsTest.java
@@ -5,10 +5,18 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MyArrayUtilsTest {
-
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void many() {
+		int[] array = {1,2,3};
+		int highest = MyArrayUtils.findHighest(array);
+		assertEquals(highest, 3);
 	}
-
+	@Test
+	public void empty() {
+		int[] array = {};
+		int highest = MyArrayUtils.findHighest(array);
+		assertEquals(highest, Integer.MIN_VALUE);
+	}
 }
