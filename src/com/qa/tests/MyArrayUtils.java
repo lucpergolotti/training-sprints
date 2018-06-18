be16f10ade9b35350beeb5a9560cc57b973c0f6f
Luc <lucpergolotti@gmail.com>
2018-06-18 14:05:52 -0400

    Test one and two length arrays

--- src/com/qa/tests/MyArrayUtils.java
+++ src/com/qa/tests/MyArrayUtils.java
@@ -1,9 +1,21 @@
 package com.qa.tests;
 
 public class MyArrayUtils {
-	public static int findHighest(int[] numbers) {
+	
+	public static int findHighest(int[] nums) {
 		
-		
+		if(nums.length == 1) {
+			return nums[0];
+		}
+		if(nums.length == 2) {
+			if(nums[0] > nums[1]) {
+				return nums[0];
+			} else {
+				return nums[1];
+			}
+		}
 		return 0;
+		
+		
 	}
 }
--- src/com/qa/tests/MyArrayUtilsTest.java
+++ src/com/qa/tests/MyArrayUtilsTest.java
@@ -1,14 +1,30 @@
 package com.qa.tests;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MyArrayUtilsTest {
 
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void findHighestInArrayOfOne() {
+		int[] a1 = new int[1];
+		a1[0] = 10;
+		assertThat(MyArrayUtils.findHighest(a1), is(10));
+		
 	}
+	@Test
+	public void findHighestInArrayOfTwo() {
+		int[] a1 = new int[]{10, 20};
+		assertThat(MyArrayUtils.findHighest(a1), is(20));
+		
+		
+	}
+
 
 }
